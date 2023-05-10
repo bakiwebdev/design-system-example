b13e6043fc13eac4fc768cad767942e512d0945d
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-05-10 17:25:41 +0200

    Implement Design System for button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,72 @@
+"use client";
+import Button from "@/components/Button";
+import { useState } from "react";
+
 export default function Home() {
+  const [buttonStyle, setButtonStyle] = useState<
+    "default" | "outline" | "text"
+  >("default");
+  const [buttonSize, setButtonSize] = useState<"small" | "medium" | "large">(
+    "medium"
+  );
+  const [buttonType, setButtonType] = useState<
+    "primary" | "affirmative" | "negative"
+  >("primary");
+  const Select = (
+    value: string,
+    // rome-ignore lint/suspicious/noExplicitAny: <explanation>
+    onChange: (size: any) => void,
+    options: string[]
+  ) => {
+    return (
+      <select
+        className="block appearance-none w-full max-w-fit bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+        value={value}
+        onChange={(event) => {
+          onChange(event?.target?.value || "");
+        }}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    );
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="bg-green-500">This is Tailwind CSS & Next JS</h1>
+    <main className="w-screen h-screen mx-auto">
+      <section className="w-full h-full flex flex-col justify-center items-center">
+        <Button size={buttonSize} type={buttonType} style={buttonStyle}>
+          Click me
+        </Button>
+        <div className="flex gap-2 m-2">
+          {/* size dropdown */}
+          {Select(
+            buttonSize,
+            (size: "small" | "medium" | "large") => {
+              setButtonSize(size);
+            },
+            ["small", "medium", "large"]
+          )}
+          {/* style dropdown */}
+          {Select(
+            buttonStyle,
+            (style: "text" | "default" | "outline") => {
+              setButtonStyle(style);
+            },
+            ["text", "default", "outline"]
+          )}
+          {/* type dropdown */}
+          {Select(
+            buttonType,
+            (type: "primary" | "affirmative" | "negative") => {
+              setButtonType(type);
+            },
+            ["primary", "affirmative", "negative"]
+          )}
+        </div>
+      </section>
     </main>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,60 @@
+"use client";
+import clsx from "clsx";
+
+interface IButton {
+  children: React.ReactNode;
+  size?: "small" | "medium" | "large";
+  style?: "default" | "outline" | "text";
+  type?: "primary" | "affirmative" | "negative";
+}
+
+const Button = ({ children, size, style, type }: IButton) => {
+  return (
+    <button
+      className={clsx(
+        "cursor-pointer text-white px-5 py-2 rounded-md transform transition-colors first-letter:capitalize",
+        {
+          // deal with button size
+          "text-sm": size === "small", // small
+          "text-md": size === "medium", // medium
+          "text-lg": size === "large", // large
+        },
+        {
+          // deal with button type
+          "bg-blue-500 border border-blue-500 hover:bg-blue-600":
+            type === "primary", // primary
+          "bg-red-500 border border-red-500 hover:bg-red-600":
+            type === "negative", // negative
+          "bg-green-500 border border-green-500 hover:bg-green-600":
+            type === "affirmative", // affirmative
+        },
+        {
+          // deal with button style
+          "": style === "default", //default
+          //
+          //
+          "bg-transparent hover:bg-transparent": style === "outline", //outline
+          "text-blue-500 hover:border-blue-600 hover:text-blue-600":
+            style === "outline" && type === "primary", // outline & primary
+          "text-red-500 hover:border-red-600 hover:text-red-600":
+            style === "outline" && type === "negative", // outline & negative
+          "text-green-500 hover:border-green-600 hover:text-green-600":
+            style === "outline" && type === "affirmative", // outline & affirmative
+          //
+          //
+          "border-0 bg-transparent": style === "text", //text
+          "text-blue-500 hover:bg-blue-50":
+            style === "text" && type === "primary", // outline & primary
+          "text-red-500 hover:bg-red-50":
+            style === "text" && type === "negative", // outline & negative
+          "text-green-500 hover:bg-green-50":
+            style === "text" && type === "affirmative", // outline & affirmative
+        }
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
