b75754857145e054f30401a162a7d2d1d95ebb7f
Biruk Endris <57604289+bakiwebdev@users.noreply.github.com>
2023-05-10 14:19:19 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="bg-green-500">This is Tailwind CSS & Next JS</h1>
+    </main>
+  );
+}
